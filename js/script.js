a9bf0fc22420d03666ba57dc4a89d80c4190f633
MiyazakiaAsami <115138320+MiyazakiaAsami@users.noreply.github.com>
2022-11-29 15:10:07 +0900

    a

--- js/script.js
+++ js/script.js
@@ -14,6 +14,24 @@ function toggleNav() {
   }
   toggleNav();
 
+//スライダー
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop:true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 //教室のようすスライドショー
 $('.slider').slick({
